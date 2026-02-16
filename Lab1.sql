2d653c3b4303f2aa2c198fb737431a3b10e15817
dominikenzenhofer <dominike@ppedv.de>
2026-02-16 12:04:01 +0100

    lab01

---
+++ Lab1.sql
@@ -0,0 +1,31 @@
+select 100 as Zahl,
+	   'Donaudampfschifffahrtsgesellschaft' as Text,
+	   200 as Rechnung
+
+select CustomerID as KundenID, 
+		CompanyName as Firmenname, 
+		ContactName as Kontaktperson, 
+		Phone as Telefonnummer 
+		from Customers
+
+select ProductID as ProduktID, 
+		ProductName as Pruduktname, 
+		UnitPrice as Stückpreis 
+		from Products
+
+select Freight as Nettofrachtkosten,
+		Freight * 1.19 as Bruttofrachtkosten,
+		Freight * 0.19 as MwSt from Orders
+
+-- auf 2 Dezimalstellen Runden mit Round 
+select Freight as Nettofrachtkosten,
+		ROUND(Freight * 1.19, 2) as Bruttofrachtkosten,
+		ROUND(Freight * 0.19, 2) as MwSt from Orders
+
+-- auf 2 Dezimalstellen Runden und 0 entfernen mit str
+select Freight as Nettofrachtkosten,
+		STR(Freight * 1.19, 10, 2) as Bruttofrachtkosten,
+		STR(Freight * 0.19, 10, 2) as MwSt from Orders
+		-- STR(Spalte, Länge, Anzahl der Dezimalstellen) 
+		-- gibt die Zahl als String zurück, wobei die Länge die Gesamtzahl der Zeichen angibt, 
+		-- einschließlich Dezimalpunkt und Vorzeichen.
